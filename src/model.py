34f49fdc63dd05e357ef7ee899f9301be23d7a92
Sayam Gupta <gupta.sayam2006@gmail.com>
2023-06-27 08:32:43 +0530

    Finished Model

--- src/model.py
+++ src/model.py
@@ -2,14 +2,13 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+
 
 
 
 
 data = pd.read_csv("data/games1.csv")
-categorical_columns = ["opening", "average_rating"]
+categorical_columns = ["opening"]
 
 encoder = OneHotEncoder(sparse=False)
 encoder.fit(data[categorical_columns])
@@ -29,7 +28,7 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape = X_train.shape[1:]) )
-model.add(tf.keras.layers.Dense(256, activation = "relu"))
+model.add(tf.keras.layers.Dense(256, activation = "sigmoid"))
 model.add(tf.keras.layers.Dense(2301))
 
 model.compile(optimizer = "adam", loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True), metrics = ["accuracy"])
